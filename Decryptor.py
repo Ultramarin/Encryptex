ad8b672358cb05e6b063f03efd766b81207932df
Ultramarin <ultramarin116@yandex.ua>
2014-03-20 23:56:15 +0200

    Создана функция ввода и проверки зашифрованного текста, доделана функция переобразования вторичного ключа в лист кординат

--- Decryptor.py
+++ Decryptor.py
@@ -8,12 +8,13 @@ def user_diag_encripted_text():
     """
     while True:
         ET = raw_input('Введите зашыврований текст')
-        if len(ET) >169 and ET not in  helpers.keytablegen(ET, leng='eng'):
+        if len(ET) > 169 and ET not in helpers.keytablegen(ET, leng='eng'):
             print ('')
         else:
             break
 
     return ET
+
 
 def user_diag_EK_decrypt(max_int_const=26):
     """
@@ -31,20 +32,14 @@ def user_diag_EK_decrypt(max_int_const=26):
         if helpers.EK_check(encrypt_key):
             break
         else:
-                print answer1
+            print answer1
 
     return encrypt_key
 
 
 def user_diag_encode_key(lang='eng', const=338):
-    #TODO: Сделать функцию автономной
-    #TODO: Отредактировать функцию key_in_alfabet в helpers. Подогнав ее под русский и англиский алфавит с доп. переменной
-
-
-
     while True:
         encode_key = raw_input('Введите дешифровочный ключ(вторичный)')
-        #TODO: Числа должны быть константами, указанными в теле  функции
         if len(encode_key) > const or encode_key not in helpers.key_in_alfabet(encode_key, lang):
             print ('Дешифровочный ключ не верен')
         else:
@@ -52,17 +47,46 @@ def user_diag_encode_key(lang='eng', const=338):
 
 
 def EK_parser():
-
+    """
+    Переобразования дешифровачного ключа  вводимого пользователем в лист кординат
+    зашифрованого текста
+    """
     encrypt_key = user_diag_EK_decrypt()
     encrypt_key_x = encrypt_key[:13]
     encrypt_key_y = encrypt_key[13:]
-    encode_key  = user_diag_encode_key()
+    encrypt_key_dict = {}
+    encode_key = user_diag_encode_key()
     encode_key_list_xy = []
-    cordinates_list = []
+    cordinates_list_letter = []
+    cantor = 0
+    coordinates_list = []
 
-    for x in encode_key:
-        pass
+    while cantor <= len(encode_key):
+        encode_key_x = encode_key[cantor]
+        encode_key_y = encode_key[cantor + 1]
+        encode_key_list_xy.append(encode_key_x)
+        encode_key_list_xy.append(encode_key_y)
+        cordinates_list_letter.append(encode_key_list_xy)
+        cantor += 1
 
+    cantor = 0
 
+    for x in encrypt_key_x:
+        encrypt_key_dict[x] = cantor
+        cantor += 1
 
+    cantor = 0
+    encode_key_list_xy = []
 
+    for x in encrypt_key_y:
+        encrypt_key_dict[cantor] = x
+        cantor += 1
+
+    for x in cordinates_list_letter:
+        x_coordinated = encrypt_key_dict[x[0]]
+        y_coordinated = encrypt_key_dict[x[1]]
+        encode_key_list_xy.append(x_coordinated)
+        encode_key_list_xy.append(y_coordinated)
+        coordinates_list.append(encode_key_list_xy)
+
+    return coordinates_list
